14c977316768c788e4811af3d2be5d40e8330f7b
sko00o <lwylwy@outlook.com>
2022-11-25 17:57:20 +0800

    add a demo for kafka

---
+++ kafka/README.md
@@ -0,0 +1,9 @@
+# How to connect Kafka in docker?
+
+- https://stackoverflow.com/a/51634499
+
+## client and broker on same host
+
+```sh
+cd client-on-same-host && ../run.sh
+```
---
+++ kafka/client-on-same-host/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo ">> compose environment"
+docker compose up -d --remove-orphans
+
+echo ">> wait for some transmition"
+sleep 10
+
+echo ">> show some logs"
+docker compose logs --timestamps --since=10s --tail=5 consumer producer
+
+docker compose down --remove-orphans
