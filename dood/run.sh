588d3928bd79faf161d7421d406dd5bb1d10260c
sko00o <lwylwy@outlook.com>
2022-11-25 11:23:30 +0800

    Docker outside of Docker

---
+++ dood/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo ">> compose environment"
+docker compose up -d --remove-orphans
+
+echo ">> show docker info by Docker-CLI"
+docker compose exec -it docker-client \
+    docker info
+
+echo ">> show docker info by API"
+# API Reference: https://docs.docker.com/engine/api/v1.41/
+docker compose exec -it curl \
+    curl --unix-socket /var/run/docker.sock http://localhost/v1.41/info
+
+docker compose down --remove-orphans
