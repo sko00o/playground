b7c90c8a2d5d955367ea08813c0650f69d969b28
sko00o <lwylwy@outlook.com>
2022-12-01 09:53:29 +0800

    add devcontainer

--- kafka/client-on-same-machine/run.sh
+++ kafka/client-on-same-machine/run.sh
@@ -9,13 +9,18 @@ sleep 10
 echo ">> show some logs"
 docker compose logs --timestamps --since=10s --tail=5 consumer producer
 
-if [ -x "$(command -v kcat)" ]; then
+for cmd in kcat kafkacat; do
+if [ -x "$(command -v $cmd)" ]; then
+    echo ">> found $cmd"
     echo ">> produce 10 messages using kcat"
     for i in {00..10}; do printf "%02d\n" $i; done |
-        kcat -P -b localhost:19092 -t test1
+        $cmd -P -b localhost:19092 -t test1
 
     echo ">> consume 10 messages using kcat"
-    kcat -C -b localhost:19092 -t test1 -o -10 -e
+    $cmd -C -b localhost:19092 -t test1 -o -10 -e
+
+    break
 fi
+done
 
 docker compose down --remove-orphans
