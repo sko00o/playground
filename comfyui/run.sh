33eb397490f700ceee47cfca701c382147dc4bca
Wenyu.Li <lwylwy@outlook.com>
2024-12-06 06:54:29 +0000

    add healthcheck

--- comfyui/run.sh
+++ comfyui/run.sh
@@ -4,19 +4,14 @@ set -e
 cd "$(dirname "$0")"
 
 echo ">> compose environment"
-docker compose up -d --remove-orphans
+docker compose up -d --build --remove-orphans
 
 defer() {
     docker compose down --remove-orphans --volumes
     exit 0
 }
+trap defer EXIT
 
-echo "Please open http://127.0.0.1:8188"
-
-trap defer SIGINT
-
-# Wait for Ctrl+C
-while true; do
-    sleep 1
-done
-
+echo "Please open http://127.0.0.1:8188 in browser."
+echo "Now exec any commands in container: (Enter Ctrl+D to quit.)"
+docker compose exec -it comfyui bash
