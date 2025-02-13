fedef083b1392045ee93c87b6ca9383e019db269
Li Wenyu <lwylwy@outlook.com>
2025-02-13 16:50:02 +0800

    fix wg demo

--- wireguard/run.sh
+++ wireguard/run.sh
@@ -2,15 +2,25 @@
 
 cd "$(dirname "$0")"
 
-echo ">> compose environment"
-docker compose up -d --remove-orphans
-
 function cleanup {
-    docker compose down --remove-orphans --volumes
-    exit 0
+  docker compose down --remove-orphans --volumes
+  exit 0
 }
 trap 'cleanup' SIGINT
 trap 'cleanup' SIGTERM
+
+echo ">> compose environment"
+docker compose up -d --remove-orphans
+
+echo ">> show wg-client interfaces"
+docker compose exec wg-client wg show
+
+echo ">> show wg-client ip routes"
+docker compose exec wg-client ip route show
+
+echo ">> test ping wg-server from wg-client"
+docker compose exec wg-client ping -c 4 10.13.13.1
+
 echo "press ctrl+c to stop"
 
 # Wait for signals
